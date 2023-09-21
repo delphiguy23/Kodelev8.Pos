336851964c1bda8996d0e3831e5854baa8fbaab2
delphiguy23 <delphiguy23@gmail.com>
2023-09-21 12:09:06 +0800

    Updated Supplier Handlers

--- Point.Of.Sale.Supplier/Handlers/Command/LinkToTenant/LinkToTenantCommandHandler.cs
+++ Point.Of.Sale.Supplier/Handlers/Command/LinkToTenant/LinkToTenantCommandHandler.cs
@@ -1,7 +1,10 @@
+using Microsoft.Extensions.Logging;
 using Point.Of.Sale.Abstraction.Message;
 using Point.Of.Sale.Persistence.UnitOfWork;
+using Point.Of.Sale.Retries.RetryPolicies;
 using Point.Of.Sale.Shared.FluentResults;
 using Point.Of.Sale.Supplier.Repository;
+using Polly;
 
 namespace Point.Of.Sale.Supplier.Handlers.Command.LinkToTenant;
 
@@ -9,21 +12,27 @@ public class LinkToTenantCommandHandler : ICommandHandler<LinkToTenantCommand>
 {
     private readonly IRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger _logger;
 
-    public LinkToTenantCommandHandler(IRepository repository, IUnitOfWork unitOfWork)
+    public LinkToTenantCommandHandler(IRepository repository, IUnitOfWork unitOfWork, ILogger logger)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _logger = logger;
     }
 
     public async Task<IFluentResults> Handle(LinkToTenantCommand request, CancellationToken cancellationToken)
     {
-        var result = await _repository.LinkToTenant(new Shared.Models.LinkToTenant
+        var result = await PosPolicies.ExecuteThenCaptureResult(() => _repository.LinkToTenant(new Shared.Models.LinkToTenant
         {
             TenantId = request.tenantId,
             EntityId = request.entityId,
-        }, cancellationToken);
+        }, cancellationToken), _logger);
 
-        return ResultsTo.Something(result);
+        return result switch
+        {
+            {Result: null, Outcome: OutcomeType.Failure} => ResultsTo.Failure<string>().FromException(result.FinalException),
+            _ => ResultsTo.Something(result.Result!.Value),
+        };
     }
 }
--- Point.Of.Sale.Supplier/Handlers/Command/Update/UpdateCommandHandler.cs
+++ Point.Of.Sale.Supplier/Handlers/Command/Update/UpdateCommandHandler.cs
@@ -1,25 +1,29 @@
+using Microsoft.Extensions.Logging;
 using Point.Of.Sale.Abstraction.Message;
 using Point.Of.Sale.Persistence.UnitOfWork;
+using Point.Of.Sale.Retries.RetryPolicies;
 using Point.Of.Sale.Shared.FluentResults;
-using Point.Of.Sale.Shared.FluentResults.Extension;
 using Point.Of.Sale.Supplier.Repository;
+using Polly;
 
 namespace Point.Of.Sale.Supplier.Handlers.Command.Update;
 
 public class UpdateCommandHandler : ICommandHandler<UpdateCommand>
 {
+    private readonly ILogger<UpdateCommandHandler> _logger;
     private readonly IRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public UpdateCommandHandler(IRepository repository, IUnitOfWork unitOfWork)
+    public UpdateCommandHandler(IRepository repository, IUnitOfWork unitOfWork, ILogger<UpdateCommandHandler> logger)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _logger = logger;
     }
 
     public async Task<IFluentResults> Handle(UpdateCommand request, CancellationToken cancellationToken)
     {
-        var result = await _repository.Update(new Persistence.Models.Supplier
+        var result = await PosPolicies.ExecuteThenCaptureResult(() => _repository.Update(new Persistence.Models.Supplier
         {
             Id = request.Id,
             TenantId = request.TenantId,
@@ -33,18 +37,14 @@ public class UpdateCommandHandler : ICommandHandler<UpdateCommand>
             UpdatedOn = DateTime.UtcNow,
             UpdatedBy = "User",
             Active = request.Active,
-        }, cancellationToken);
+        }, cancellationToken), _logger);
 
-        if (result.IsNotFound())
+        return result switch
         {
-            return ResultsTo.NotFound().WithMessage("Supplier Not Found");
-        }
-
-        if (result.Value.Count == 0)
-        {
-            return ResultsTo.NotFound().WithMessage("Supplier not updated");
-        }
-
-        return ResultsTo.Something(result.Value.Entity);
+            {Result: null, Outcome: OutcomeType.Failure} => ResultsTo.Failure<string>().FromException(result.FinalException),
+            {Result.Status: FluentResultsStatus.NotFound} => ResultsTo.NotFound().WithMessage("Supplier Not Found"),
+            {Result.Value.Count: 0} => ResultsTo.NotFound().WithMessage("Supplier not updated"),
+            _ => ResultsTo.Something(result.Result!.Value.Count > 0),
+        };
     }
 }
